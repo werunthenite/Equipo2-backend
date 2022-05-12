fbc9d8ee51a1e9c0bda1c23c0e1e0b42c789a4d4
Alejandro Hernandez <werunthenite@gmail.com>
2022-05-12 12:39:09 -0500

    Credentials added

--- dockerfile
+++ dockerfile
@@ -4,7 +4,7 @@ COPY . /usr/src/app
 RUN mvn package
 
 FROM adoptopenjdk/openjdk11:alpine-jre
-EXPOSE 8080
+EXPOSE 8081
 ARG JAR_FILE=/usr/src/app/target/azul-0.0.1-SNAPSHOT.jar
 WORKDIR /opt/app
 COPY --from=maven ${JAR_FILE} app.jar
--- src/main/java/com/itesm/azul/config/AzulConfig.java
+++ src/main/java/com/itesm/azul/config/AzulConfig.java
@@ -8,10 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -52,6 +49,11 @@ public class AzulConfig {
     */
 @Bean
 public DynamoDbClient getDynamoDbClient() {
+    AwsSessionCredentials awsCreds = AwsSessionCredentials.create(
+            "ASIATZZ7OPKK3WDCMF5T",
+            "VRo8t8WHroRxAdXN0EwAfSIl4Sgg79IUgdSMv/5V",
+            "FwoGZXIvYXdzEIL//////////wEaDGh8NlKX9nFroGSipCLFAXTMsZyH11+HDGjzzZhHoEpQP/DMXOzItkYdjqPDiNWH7VRxT6Tf4ftJIZhk3kL8KSsLxtBVx76FVK0IrOU6S1miJoKTi4lqvUJnTHtZKPGotW+Jx1LzwjohWSt2qT75SVGsVzivInEjhgL3/+TwKnqhy/OEGVMnYFTszyAcbhcDugk6zYK8UAf2rDMEDdiiYft7u/JBjs7wSLnavWIbCQ1TOH2G1xKi1I3mD3lnVwSOxhBXEI1CV/WkyCbNEpVGItscgL2hKMDt9JMGMi2KDgkaRClZmKzsAf6vAxAhkuIyHKRVJ/VoNVUt1S7cOY7rStfuyXV/YuDmNYc=");
+
     AwsCredentialsProvider credentialsProvider =
             DefaultCredentialsProvider.builder()
                     .profileName("default")
@@ -59,7 +61,7 @@ public DynamoDbClient getDynamoDbClient() {
 
     return DynamoDbClient.builder()
             .region(Region.US_EAST_1)
-            .credentialsProvider(credentialsProvider).build();
+            .credentialsProvider(StaticCredentialsProvider.create(awsCreds)).build();
 }
 
     @Bean
